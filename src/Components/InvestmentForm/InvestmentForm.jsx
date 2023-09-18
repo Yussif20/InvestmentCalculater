a3faac01dd891131125f116ebcfd7e0fd43dcbcc
Yousef <yousefayman2000500@gmail.com>
2023-09-18 20:22:42 +0300

    managing state and doing it all in a general state

--- src/Components/InvestmentForm/InvestmentForm.jsx
+++ src/Components/InvestmentForm/InvestmentForm.jsx
@@ -1,50 +1,66 @@
 import styles from "./InvestmentForm.module.css";
 import { useState } from "react";
 
-const InvestmentForm = () => {
-  const [currentSavings, setCurrentSavings] = useState("");
-  const [yearlySavings, setYearlySavings] = useState("");
-  const [expectedInterest, setExpectedInterest] = useState("");
-  const [investmentDuration, setInvestmentDuration] = useState("");
+let initialData = {
+  "current-savings": 1000,
+  "yearly-contribution": 1500,
+  "expected-return": 2000,
+  duration: 2500,
+};
 
-  const currentSavingsHandler = (e) => {
-    setCurrentSavings(e.target.value);
-  };
-  const yearlySavingsHandler = (e) => {
-    setYearlySavings(e.target.value);
-  };
-  const expectedInterestHandler = (e) => {
-    setExpectedInterest(e.target.value);
-  };
-  const investmentDurationHandler = (e) => {
-    setInvestmentDuration(e.target.value);
+const InvestmentForm = () => {
+  // const [currentSavings, setCurrentSavings] = useState("");
+  // const [yearlySavings, setYearlySavings] = useState("");
+  // const [expectedInterest, setExpectedInterest] = useState("");
+  // const [investmentDuration, setInvestmentDuration] = useState("");
+
+  // const currentSavingsHandler = (e) => {
+  //   setCurrentSavings(e.target.value);
+  // };
+  // const yearlySavingsHandler = (e) => {
+  //   setYearlySavings(e.target.value);
+  // };
+  // const expectedInterestHandler = (e) => {
+  //   setExpectedInterest(e.target.value);
+  // };
+  // const investmentDurationHandler = (e) => {
+  //   setInvestmentDuration(e.target.value);
+  // };
+  const [userInput, setUserInput] = useState(initialData);
+
+  const inputChangeHandler = (input, value) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: value,
+      };
+    });
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
-    const investmentData = {
-      curSavings: currentSavings,
-      yearSavings: yearlySavings,
-      expectInterest: expectedInterest,
-    };
   };
   const resetHandler = (e) => {
     e.preventDefault();
-    setCurrentSavings("");
-    setYearlySavings("");
-    setExpectedInterest("");
-    setInvestmentDuration("");
+    setUserInput(initialData);
   };
+
   return (
-    <form className={styles.form}>
+    <form
+      className={styles.form}
+      onSubmit={submitHandler}
+      onReset={resetHandler}
+    >
       <div className={styles["input-group"]}>
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
           <input
             type="number"
             id="current-savings"
-            onChange={currentSavingsHandler}
-            value={currentSavings}
+            onChange={(e) => {
+              inputChangeHandler("current-savings", e.target.value);
+            }}
+            value={userInput["current-savings"]}
           />
         </p>
         <p>
@@ -52,8 +68,10 @@ const InvestmentForm = () => {
           <input
             type="number"
             id="yearly-contribution"
-            onChange={yearlySavingsHandler}
-            value={yearlySavings}
+            onChange={(e) => {
+              inputChangeHandler("yearly-contribution", e.target.value);
+            }}
+            value={userInput["yearly-contribution"]}
           />
         </p>
       </div>
@@ -65,8 +83,10 @@ const InvestmentForm = () => {
           <input
             type="number"
             id="expected-return"
-            onChange={expectedInterestHandler}
-            value={expectedInterest}
+            onChange={(e) => {
+              inputChangeHandler("expected-return", e.target.value);
+            }}
+            value={userInput["expected-return"]}
           />
         </p>
         <p>
@@ -74,24 +94,18 @@ const InvestmentForm = () => {
           <input
             type="number"
             id="duration"
-            onChange={investmentDurationHandler}
-            value={investmentDuration}
+            onChange={(e) => {
+              inputChangeHandler("duration", e.target.value);
+            }}
+            value={userInput["duration"]}
           />
         </p>
       </div>
       <p className="actions">
-        <button
-          type="reset"
-          className={styles.buttonAlt}
-          onReset={resetHandler}
-        >
+        <button type="reset" className={styles.buttonAlt}>
           Reset
         </button>
-        <button
-          type="submit"
-          className={styles.button}
-          onSubmit={submitHandler}
-        >
+        <button type="submit" className={styles.button}>
           Calculate
         </button>
       </p>
