ade35329c35fffaf5d35932679dd67373ea7d5fb
Yousef <yousefayman2000500@gmail.com>
2023-09-18 04:10:16 +0300

    adding the states and the changes handler

--- src/Components/InvestmentForm/InvestmentForm.jsx
+++ src/Components/InvestmentForm/InvestmentForm.jsx
@@ -1,16 +1,43 @@
 import styles from "./InvestmentForm.module.css";
+import { useState } from "react";
 
 const InvestmentForm = () => {
+  const [currentSavings, setCurrentSavings] = useState("");
+  const [yearlySavings, setYearlySavings] = useState("");
+  const [expectedInterest, setExpectedInterest] = useState("");
+  const [investmentDuration, setInvestmentDuration] = useState("");
+
+  const currentSavingsHandler = (e) => {
+    setCurrentSavings(e.target.value);
+  };
+  const yearlySavingsHandler = (e) => {
+    setYearlySavings(e.target.value);
+  };
+  const expectedInterestHandler = (e) => {
+    setExpectedInterest(e.target.value);
+  };
+  const investmentDurationHandler = (e) => {
+    setInvestmentDuration(e.target.value);
+  };
+
   return (
     <form className={styles.form}>
       <div className={styles["input-group"]}>
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            id="current-savings"
+            onChange={currentSavingsHandler}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            onChange={yearlySavingsHandler}
+          />
         </p>
       </div>
       <div className={styles["input-group"]}>
@@ -18,11 +45,19 @@ const InvestmentForm = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            onChange={expectedInterestHandler}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            type="number"
+            id="duration"
+            onChange={investmentDurationHandler}
+          />
         </p>
       </div>
       <p className="actions">
